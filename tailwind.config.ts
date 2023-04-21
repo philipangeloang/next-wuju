5cfd9f9e30a56fb5ea9c78ec61fbfd1b731716d9
philipangeloang <philipangeloang1@gmail.com>
2023-04-21 21:12:45 +0800

    Initial Log in Design

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,16 @@ import { type Config } from "tailwindcss";
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        poppins: "Poppins",
+        dmsans: "DM Serif Display"
+      },
+      colors: {
+        "main-black": "#252525",
+        "off-white": "#f2f2f2"
+      },
+    },
   },
   plugins: [],
 } satisfies Config;
